a4cbe8ae1b5ae209ed8a3fcb23c1d52d630f34f0
Vrishank <imvrishank@gmail.com>
2025-08-25 13:16:24 +0530

    Create hfxh.cpp

---
+++ hfxh.cpp
@@ -0,0 +1,223 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP_Mail_Client.h>
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "Admin_only";
+const char* password = "Subhash3pati@123";
+
+// To send Emails using Gmail on port 465 (SSL), you need to create an app password: https://support.google.com/accounts/answer/185833
+#define emailSenderAccount    "whyvrishank@gmail.com"
+#define emailSenderPassword   "Vrishank2006*"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465
+#define emailSubject          "[ALERT] ESP32 Temperature"
+
+// Default Recipient Email Address
+String inputMessage = "eeriebaguette@gmail.com";
+String enableEmailChecked = "checked";
+String inputMessage2 = "true";
+// Default Threshold Temperature Value
+String inputMessage3 = "25.0";
+String lastTemperature;
+
+// HTML web page to handle 3 input fields (email_input, enable_email_input, threshold_input)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Email Notification with Temperature</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <h2>DS18B20 Temperature</h2> 
+  <h3>%TEMPERATURE% C</h3>
+  <h2>ESP Email Notification</h2>
+  <form action="/get">
+    Email Address <input type="email" name="email_input" value="%EMAIL_INPUT%" required><br>
+    Enable Email Notification <input type="checkbox" name="enable_email_input" value="true" %ENABLE_EMAIL%><br>
+    Temperature Threshold <input type="number" step="0.1" name="threshold_input" value="%THRESHOLD%" required><br>
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+AsyncWebServer server(80);
+
+// Replaces placeholder with DS18B20 values
+String processor(const String& var){
+  if(var == "TEMPERATURE"){
+    return lastTemperature;
+  }
+  else if(var == "EMAIL_INPUT"){
+    return inputMessage;
+  }
+  else if(var == "ENABLE_EMAIL"){
+    return enableEmailChecked;
+  }
+  else if(var == "THRESHOLD"){
+    return inputMessage3;
+  }
+  return String();
+}
+
+// Flag variable to keep track if email notification was sent or not
+bool emailSent = false;
+
+const char* PARAM_INPUT_1 = "email_input";
+const char* PARAM_INPUT_2 = "enable_email_input";
+const char* PARAM_INPUT_3 = "threshold_input";
+
+// Interval between sensor readings
+unsigned long previousMillis = 0;     
+const long interval = 5000;    
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// SMTP session object
+SMTPSession smtp;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println();
+  Serial.print("ESP IP Address: http://");
+  Serial.println(WiFi.localIP());
+  
+  // Start the DS18B20 sensor
+  sensors.begin();
+
+  // Send web page to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html, processor);
+  });
+
+  // Receive an HTTP GET request at <ESP_IP>/get?email_input=<inputMessage>&enable_email_input=<inputMessage2>&threshold_input=<inputMessage3>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      if (request->hasParam(PARAM_INPUT_2)) {
+        inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+        enableEmailChecked = "checked";
+      }
+      else {
+        inputMessage2 = "false";
+        enableEmailChecked = "";
+      }
+      if (request->hasParam(PARAM_INPUT_3)) {
+        inputMessage3 = request->getParam(PARAM_INPUT_3)->value();
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    Serial.println(inputMessage2);
+    Serial.println(inputMessage3);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP.<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+
+  // Initialize ESP-Mail-Client
+  MailClient.networkReconnect(true);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    sensors.requestTemperatures();
+    // Temperature in Celsius degrees 
+    float temperature = sensors.getTempCByIndex(0);
+    Serial.print(temperature);
+    Serial.println(" *C");
+    
+    lastTemperature = String(temperature);
+    
+    // Check if temperature is above threshold and if it needs to send the Email alert
+    if(temperature > inputMessage3.toFloat() && inputMessage2 == "true" && !emailSent){
+      String emailMessage = String("Temperature above threshold. Current temperature: ") + 
+                            String(temperature) + String("C");
+      if(sendEmailNotification(emailMessage)) {
+        Serial.println(emailMessage);
+        emailSent = true;
+      }
+      else {
+        Serial.println("Email failed to send");
+      }    
+    }
+    // Check if temperature is below threshold and if it needs to send the Email alert
+    else if((temperature < inputMessage3.toFloat()) && inputMessage2 == "true" && emailSent) {
+      String emailMessage = String("Temperature below threshold. Current temperature: ") + 
+                            String(temperature) + String(" C");
+      if(sendEmailNotification(emailMessage)) {
+        Serial.println(emailMessage);
+        emailSent = false;
+      }
+      else {
+        Serial.println("Email failed to send");
+      }
+    }
+  }
+}
+
+bool sendEmailNotification(String emailMessage) {
+  // Configure the session
+  ESP_Mail_Session session;
+  session.server.host_name = smtpServer;
+  session.server.port = smtpServerPort;
+  session.login.email = emailSenderAccount;
+  session.login.password = emailSenderPassword;
+  session.login.user_domain = "";
+
+  // Configure the message
+  SMTP_Message message;
+  message.sender.name = "ESP32";
+  message.sender.email = emailSenderAccount;
+  message.subject = emailSubject;
+  message.addRecipient("Recipient", inputMessage);
+  message.text.content = emailMessage.c_str();
+  message.text.charSet = "utf-8";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_high;
+  // Set the callback function
+  smtp.callback(smtpCallback);
+
+  // Connect to the server and send the email
+  if (!smtp.connect(&session)) {
+    Serial.println("Failed to connect to SMTP server: " + smtp.errorReason());
+    return false;
+  }
+
+  if (!MailClient.sendMail(&smtp, &message)) {
+    Serial.println("Error sending Email: " + smtp.errorReason());
+    smtp.closeSession();
+    return false;
+  }
+
+  smtp.closeSession();
+  return true;
+}
+
+// Callback function for email sending status
+void smtpCallback(SMTP_Status status) {
+  Serial.println(status.info());
+  if (status.success()) {
+    Serial.println("Email sent successfully");
+    Serial.println("----------------");
+  }
+}
